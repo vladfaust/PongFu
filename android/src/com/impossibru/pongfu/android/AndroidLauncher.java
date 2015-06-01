be777db548638dd87855359324e0e0f3313c975e
Vladislav Faust <vladislav.faust@gmail.com>
2015-06-01 17:47:05 +0300

    Initial commit

---
+++ android/src/com/impossibru/pongfu/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.impossibru.pongfu.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.impossibru.pongfu.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
